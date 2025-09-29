cf773d40ed87376f83d780f2c42e90db1540797e
Afzan20 <afzan24si@mahasiswa.pcr.ac.id>
2025-09-29 10:17:54 +0700

    sip

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\QuestionController;
 
 
 
@@ -39,3 +40,5 @@ Route::get('/matakuliah', function (){
 
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/pegawai', [PegawaiController::class, 'index']);
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
